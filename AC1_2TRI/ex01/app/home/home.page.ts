f1ffe65654aaa51728f48b6a1c2db3ab63c49aa5
Thomas-Sievers <thomas@qpix.com.br>
2024-05-10 16:18:41 -0300

    finalização do primeiro ex da ac1 do segundo tri

---
+++ AC1_2TRI/ex01/app/home/home.page.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  emprestimo = 0;
+  juros = 0;
+  meses = 0;
+  prestacaoMensal = 0;
+  total = 0;
+
+  constructor() {}
+
+  calcularPrestacaoMensal(){
+    this.prestacaoMensal = this.emprestimo * (this.juros/100)
+  }
+
+  calcularTotal(){
+    this.total = (this.prestacaoMensal * this.meses) + this.emprestimo
+  }
+
+  pinFormatterNormal(value: number) {
+    return `${value}`;
+  }
+
+  pinFormatterPorcentage(value: number) {
+    return `${value}%`;
+  }
+}
